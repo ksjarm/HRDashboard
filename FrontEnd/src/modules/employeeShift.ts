67c9ba1cfce0f2cc5f8467c45263349f76d4d6d3
Sofija Valtsifer <sovalt@ttu.ee>
2023-11-27 22:47:59 +0200

    employee data validation

--- FrontEnd/src/modules/employeeShift.ts
+++ FrontEnd/src/modules/employeeShift.ts
@@ -2,18 +2,12 @@ import { Employee } from './employee';
 import { Shift } from './shift';
 
 export interface EmployeeShift {
-    employeeId: number;
-    shiftId: number;
-    employee?: Employee;
-    shift?: Shift;
-  }
-  
-  export interface State {
-    employeeShifts: EmployeeShift[];
-  }
-  
-  
-  
-  
-  
-  
+  employeeId: number;
+  shiftId: number;
+  employee?: Employee;
+  shift: Shift;
+}
+
+export interface State {
+  employeeShifts: EmployeeShift[];
+}
--- FrontEnd/src/modules/shift.ts
+++ FrontEnd/src/modules/shift.ts
@@ -1,20 +1,19 @@
 import { EmployeeShift } from './employeeShift';
 
 export interface Shift {
-    id: number;
-    title: string;
-    date?: string;
-    startTime: string;
-    endTime: string;
-    valik: string;
-    startDate?: string;
-    endDate?: string;
-    selectedWeekDay?:string;
-    employeeShifts?: EmployeeShift[]; 
-    assignedEmployeesNames?: string[];
-  }
-  
-  export interface State {
-    shifts: Shift[];
-  }
-  
+  id: number;
+  title: string;
+  date: string;
+  startTime: string;
+  endTime: string;
+  valik: string;
+  startDate?: string;
+  endDate?: string;
+  selectedWeekDay?: string;
+  employeeShifts?: EmployeeShift[];
+  assignedEmployeesNames?: string[];
+}
+
+export interface State {
+  shifts: Shift[];
+}
